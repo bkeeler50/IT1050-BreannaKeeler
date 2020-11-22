43f559d4f36015c5ca5cdfc9aeffa1821bb8e100
Breanna Keeler <bkeeler50@gmail.com>
2020-11-22 17:34:04 -0500

    Add files via upload

---
+++ Lab05/Dog/Dog/Dog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dog
+{
+    class Dog
+    {
+        public string Name { get; set; }
+
+        // Add bark() method
+        public static void bark()
+        {
+            Console.WriteLine("{0} is Barking...", Name);
+        }
+
+
+        // Add doTrick() method
+        public static void doTrick(string trickName)
+        {
+            Console.WriteLine("{0} is so smart!  {0} is doing a(n) {1}", Name, trickName);
+        }
+     
+    }   
+}
+
---
+++ Lab05/Dog/Dog/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog();
+
+            { myDog.Name = "Fido"; }
+
+            myDog.bark();
+
+            myDog.doTrick("Fetch");
+        }
+    }
+}
